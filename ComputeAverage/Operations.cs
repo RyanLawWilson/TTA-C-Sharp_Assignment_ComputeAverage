c8128b82cedba81b2b18031e2e97811a50cf65c5
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-03-02 13:30:43 -0700

    Add project files.

---
+++ ComputeAverage/Operations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputeAverage
+{
+    public class Operations
+    {
+        public int defaultNum = 100;
+
+        public int Average(int num1, int num2 = 100)
+        {
+            return (num1 + num2) / 2;
+        }
+    }
+}
---
+++ ComputeAverage/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ComputeAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program finds the average of two numbers!");
+            System.Threading.Thread.Sleep(1000);
+
+            Operations ops = new Operations();
+
+            bool loop = true;
+            while (loop)
+            {
+                int num1 = 0;
+                // Get number 1
+                bool badNumber1 = true;
+                while (badNumber1)
+                {
+                    Console.Write("Enter integer 1:  ");
+                    try
+                    {
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        badNumber1 = false;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Enter an INTEGER, please...\n");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Unknown exception: {0}", e.GetType());
+                    }
+                }
+
+                System.Threading.Thread.Sleep(150);
+
+                // Get number 2
+                Console.Write("Enter integer 2 (optional):  ");
+                int num2 = 0;
+                try
+                {
+                    num2 = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Using default value...\n");
+                    num2 = ops.defaultNum;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unknown exception: {0}", e.GetType());
+                }
+
+                Console.WriteLine("The average of {0} and {1} is {2}\n", num1, num2, ops.Average(num1, num2));
+
+                // Go again?
+                Console.Write("Do you want to enter new numbers? (y or n) ");
+                string loopChoice = Console.ReadLine();
+                if (loopChoice != "y")
+                {
+                    loop = false;
+                }
+            }
+
+            Console.WriteLine("See ya later!");
+            Console.ReadLine();
+        }
+    }
+}
